81ffe3284beb07d40ff1da7d56dd04261868441c
Nekrod <NP17210@yandex.ru>
2022-04-18 11:14:59 +0500

    some changes(sorry)

--- modules/message_draw.py
+++ modules/message_draw.py
@@ -5,16 +5,28 @@ from pathlib import Path, PurePath
 from PIL import Image, ImageDraw, ImageFont
 
 
-def draw_message_image(text):
-    new_image = Image.new("RGBA", (1500, 1000))
+def draw_message_image(text, title):
+    bubble_padding = 50
+    width = 1024
+    height = 80 * (len(textwrap.wrap(text, 35)) + 2) + bubble_padding*2
+    new_image = Image.new("RGBA", (width, height))
     draw = ImageDraw.Draw(new_image)
-    font = ImageFont.truetype(path.join("fonts", "Roboto-Regular.ttf"), 100)
+    font = ImageFont.truetype(path.join("fonts", "Roboto-Regular.ttf"), 50)
 
-    draw.rounded_rectangle((10, 10, 1490, 990), 100, fill="black")
+    draw.rounded_rectangle((bubble_padding,
+                            bubble_padding,
+                            width-bubble_padding,
+                            height-bubble_padding), 25, fill="black")
+
+    draw.text(xy=(100, 100),
+              text=title,
+              font=font,
+              stroke_width=1,
+              fill="#00bfff")
 
     line = 0
-    for i in textwrap.wrap(text, 30):
-        draw.text((100, 100+line*100),
+    for i in textwrap.wrap(text, 35):
+        draw.text(xy=(100, 180+line*80),
                   text=i,
                   font=font)
         line += 1
@@ -22,4 +34,4 @@ def draw_message_image(text):
     return new_image
 
 
-draw_message_image("Hello, World! Hello, World! Hello, World!").show()
+draw_message_image("Helloworld!WorldisGood!Worldishello!", "Vasilyi Pupkin").show()
